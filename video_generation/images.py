c0173c4ba5d13fc7db2857dba1fac540e20245e9
Mkznd <yzundai1@gmail.com>
2024-12-03 14:04:59 +0100

    Close the buffer upon completion

--- video_generation/images.py
+++ video_generation/images.py
@@ -13,11 +13,14 @@ load_dotenv()
 
 def generate_and_write_images_and_thumbnail(script: str, length: int, dir_name: str):
     number = ceil(length * 3)
+    if not os.path.isdir(dir_name):
+        os.mkdir(dir_name)
     if not os.path.isdir(f"{dir_name}/images"):
         os.mkdir(f"{dir_name}/images")
     image_urls = generate_images(script, number)
     for i, url in enumerate(image_urls):
         image = requests.get(url)
-        open(f"{dir_name}/images/{i}.png", "wb").write(image.content)
+        with open(f"{dir_name}/images/{i}.png", "wb") as file:
+            file.write(image.content)
     print("Images generated!")
-    return number
+    return len(image_urls)
